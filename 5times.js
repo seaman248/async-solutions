b2a7ea26981490b83fb78d00a2349ef4a5ef5982
seaman248 <semen.bko@ya.ru>
2014-12-19 01:38:58 +0300

    all is bad with 5 solution, i hate this!!!

--- 5times.js
+++ 5times.js
@@ -9,41 +9,77 @@ var postOptions = {
 	};
 var getUrl = 'http://' + postOptions.hostname + ':' + postOptions.port;
 
+// async.series({
+// 	postReq: function (postCb) {
+// 		function userPlus (id, cb) {
+// 			var writeBody = JSON.stringify({'user_id':id});				
+// 			var req = http.request(postOptions, function (res) {
+// 				res.on('data', function(data){});
+// 				res.on('end', function () {
+// 					cb();						
+// 				});
+// 			});
+// 			req.on('error', cb);
+// 			req.write(writeBody);
+// 			req.end();
+// 		}
+// 		async.times(5, function (id, cb) {
+// 			userPlus(++id, function (err) {
+// 				if (err) cb(err);																									
+// 			});
+// 		}, function (err) {
+// 			if (err) console.log(err);
+// 			postCb(null, 'saved');
+// 		});		
+// 	},
+// 	getReq: function (cb) {
+// 		http.get(getUrl + '/users', function (res) {
+// 			var body = '';
+// 			res.on('data', function (data) {
+// 				body += data.toString();															
+// 			});
+// 			res.on('end', function () {
+// 				cb(null,body);					
+// 			});
+// 		}).on('error', cb);
+// 	}
+// }, function (err, result) {
+// 	if(err) return console.log(err);	
+// 	console.log(result.postReq);				
+// });
+
+
+
 async.series({
-	postReq: function (postCb) {
-		function userPlus (id, cb) {
+	postReq: function (okf) {
+		function addUsr (id, cb) {
 			var writeBody = JSON.stringify({'user_id':id});				
 			var req = http.request(postOptions, function (res) {
-				res.on('data', function(data){});
+				res.on('data', function (data) {});
 				res.on('end', function () {
-					cb();						
+					cb();
 				});
 			});
-			req.on('error', cb);
+			req.on('error', function (err) {
+				if(err) throw err;
+			});
 			req.write(writeBody);
-			req.end();
+			req.end();	
 		}
 		async.times(5, function (id, cb) {
-			userPlus(++id, function (err) {
-				if (err) cb(err);																									
+			addUsr(++id, function () {
+				okf(null, 'users');					
+			}, function (err, users) {
+				if(err) console.log(err);					
+				okf(null, users);
 			});
-		}, function (err) {
-			if (err) console.log(err);
-			postCb(null, 'saved');
-		});		
+		});
+		
 	},
-	getReq: function (cb) {
-		http.get(getUrl + '/users', function (res) {
-			var body = '';
-			res.on('data', function (data) {
-				body += data.toString();															
-			});
-			res.on('end', function () {
-				cb(null,body);					
-			});
-		}).on('error', cb);
+	getReq: function (good) {
+		good(null, 'good');
 	}
 }, function (err, result) {
-	if(err) return console.log(err);	
-	console.log(result.postReq);				
+	if (err) return console.log(err);
+	console.log(result.postReq);		
 });
